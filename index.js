0f5ee18d5457024a0d589f35b6b73435b90bc126
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-04-05 18:08:07 +0900

    add post delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+
 app.use(express.json());
 
 let persons = [
@@ -42,6 +43,36 @@ app.get('/api/persons/:id', (req, res) => {
      } else {
          res.status(404).end();
      }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(p => p.id !== id);
+
+    res.status(204).end();
+});
+
+app.post('api/persons', (req, res) => {
+    let person = req.body;
+
+    if (!person.name) {
+        return res.status(400).json({ error: "name must be given" });
+    }
+
+    if (!person.number) {
+        return res.status(400).json({ error: "number must be given" });
+    }
+
+    if (persons.find(p => p.name === person.name)) {
+        return res.status(400).json({ error: "name must be unique" });
+    }
+
+    let randomId = Math.floor(Math.random() * 900000) + 100000;
+    person = {...person, id: randomId };
+    console.log(person);
+    persons = persons.concat(person);
+
+    res.json(person);
 });
 
 const PORT = 3001;
