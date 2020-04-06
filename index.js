ea92acd799d6dd656010ebb9fe7df0c16b8ba99c
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-04-07 07:52:15 +0900

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = ('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
-
 app.use(morgan('tiny'));
 
 
