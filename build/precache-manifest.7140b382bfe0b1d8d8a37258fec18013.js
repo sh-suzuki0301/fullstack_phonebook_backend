a4e6995eb08f526e6ef9425beb1a12096b500858
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-04-12 12:07:11 +0900

    frontend build settings

---
+++ build/precache-manifest.7140b382bfe0b1d8d8a37258fec18013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc76dcc5556e1d77cae29335eabdd243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef1e5738ca5f3a4021b",
+    "url": "/static/css/main.cff00b11.chunk.css"
+  },
+  {
+    "revision": "98728afebdff84228d70",
+    "url": "/static/js/2.adaa4e54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.adaa4e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef1e5738ca5f3a4021b",
+    "url": "/static/js/main.e09575ad.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
