f28c0deb67d28ec0ae260ce43048444a9ce48039
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-04-14 20:06:04 +0900

    add mongoose-unique-validator

---
+++ models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const { MONGODB_URI } = process.env;
+const uniqueValidator = require('mongoose-unique-validator');
+
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useFindAndModify", false);
+
+console.log("connecting to", MONGODB_URI);
+
+mongoose
+    .connect(MONGODB_URI, {useNewUrlParser: true })
+    .then(result => {
+        console.log("connect to MongoDB");
+    })
+    .catch(error => {
+        console.log("error connecting to MongoDB:", error.message);
+    });
+
+
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+phonebookSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject._v;
+    }
+});
+
+module.exports = mongoose.model("Person", phonebookSchema);
